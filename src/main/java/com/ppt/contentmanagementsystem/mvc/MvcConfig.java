cd933693a77446b5d285f7630710e17e0ab129ae
alvin panganiban <alvinpanganiban22@gmail.com>
2019-10-03 17:07:27 +0800

    try cors

--- src/main/java/com/ppt/contentmanagementsystem/mvc/MvcConfig.java
+++ src/main/java/com/ppt/contentmanagementsystem/mvc/MvcConfig.java
@@ -1,11 +1,13 @@
 package com.ppt.contentmanagementsystem.mvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
@@ -14,5 +16,14 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/template").setViewName("template");
+    }
+
+    public void addCorsMapping(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:80")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
     }
 }
