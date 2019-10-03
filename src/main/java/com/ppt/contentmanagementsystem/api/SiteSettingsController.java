dd5a9f6c03eb0ee0d25e02b1bc682fd56d05ba2a
alvin panganiban <alvinpanganiban22@gmail.com>
2019-10-03 17:18:16 +0800

    added CORS

--- src/main/java/com/ppt/contentmanagementsystem/api/SiteSettingsController.java
+++ src/main/java/com/ppt/contentmanagementsystem/api/SiteSettingsController.java
@@ -3,14 +3,12 @@ package com.ppt.contentmanagementsystem.api;
 import com.ppt.contentmanagementsystem.dao.SiteSettingDAO;
 import com.ppt.contentmanagementsystem.model.SiteSetting;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:80", maxAge = 3600)
 @RestController
 public class SiteSettingsController {
 
