a21bfbf4737cdf9ba945a7a156276ca1f46e941c
alvin panganiban <alvinpanganiban22@gmail.com>
2019-10-03 16:32:47 +0800

    try cors

--- src/main/java/com/ppt/contentmanagementsystem/api/CollegeController.java
+++ src/main/java/com/ppt/contentmanagementsystem/api/CollegeController.java
@@ -5,10 +5,10 @@ import com.ppt.contentmanagementsystem.model.College;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:80", maxAge = 3600)
 @RestController
 public class CollegeController {
 
--- src/main/java/com/ppt/contentmanagementsystem/mvc/MvcConfig.java
+++ src/main/java/com/ppt/contentmanagementsystem/mvc/MvcConfig.java
@@ -1,11 +1,11 @@
 package com.ppt.contentmanagementsystem.mvc;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
@@ -14,5 +14,14 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/template").setViewName("template");
+    }
+
+    public void addCorsMapping(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:80")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
     }
 }
